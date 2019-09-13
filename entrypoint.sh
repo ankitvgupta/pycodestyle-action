1a382825d9778da404e5416796a8035893cc8872
Ankit Gupta <ankit@reverielabs.com>
2019-09-12 22:18:48 -0400

    test it out

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,22 @@ if [[ -z "$GITHUB_TOKEN" ]]; then
 	exit 1
 fi
 
-pycodestyle .
+set +e
+OUTPUT=$(pycodestyle .)
+SUCCESS=$?
+echo $OUTPUT
+set -e
+
+# If there were errors as part of linting, post a comment. Else, do nothing.
+if [ $SUCCESS -ne 0 ]; then
+  echo "Making a comment"
+  PAYLOAD=$(echo '{}' | jq --arg body "$OUTPUT" '.body = $body')
+  COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
+  echo $PAYLOAD
+  echo $COMMENTS_URL
+  #curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+else
+  echo "There were not linting issues"
+fi
+exit $SUCCESS
+
